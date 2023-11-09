c397aa9f9ac20960e89be46b02fd7c39b346d377
Gus Miller <gustavo.miller@miller-hs.com>
2023-11-09 12:49:16 -0500

    Increment tabs for readability

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,14 +4,14 @@ const { User } = require('../models');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+     await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+     await User.bulkCreate(userData, {
+          individualHooks: true,
+          returning: true,
+     });
 
-  process.exit(0);
+     process.exit(0);
 };
 
 seedDatabase();
