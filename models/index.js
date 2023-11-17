903366bbcfd051fd25bb37a16433ae64aafdfbb7
Gus Miller <gustavo.miller@miller-hs.com>
2023-11-17 00:52:48 -0500

    Continue returning errors when deploying to heroku

--- models/index.js
+++ models/index.js
@@ -7,11 +7,11 @@
  * 
  * Date : 11/9/2023 7:39:28 PM
  *******************************************************************/
-const Users = require("./user");
-const Category = require("./category");
-const Posts = require("./posts");
-const Comments = require("./comments");
-const Replies = require("./replies");
+const Users = require('./users');
+const Category = require('./category');
+const Posts = require('./posts');
+const Comments = require('./comments');
+const Replies = require('./replies');
 
 Posts.belongsTo(Category, {
      foreignKey: "category_id"
