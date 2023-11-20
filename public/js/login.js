0ba37b5419aaae875698d7c1ffcf174eee5ba890
Gus Miller <gustavo.miller@miller-hs.com>
2023-11-20 10:26:10 -0500

    Second attempt to pass assignment

--- public/js/login.js
+++ public/js/login.js
@@ -8,27 +8,34 @@
  * 
  * Date : 11/9/2023 7:39:28 PM
  *******************************************************************/
-const loginFormHandler = async (event) => {
-     event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+     const loginFormHandler = async (event) => {
+          event.preventDefault();
 
-     const email = document.querySelector('#email-login').value.trim();
-     const password = document.querySelector('#password-login').value.trim();
+          const email = document.querySelector('#email-login').value.trim();
+          const password = document.querySelector('#password-login').value.trim();
 
-     if (email && password) {
-          const response = await fetch('api/users/login', {
-               method: 'POST',
-               body: JSON.stringify({ email, password }),
-               headers: { 'Content-Type': 'application/json' },
-          });
+          if (email && password) {
+               const response = await fetch('api/users/login', {
+                    method: 'POST',
+                    body: JSON.stringify({ email, password }),
+                    headers: { 'Content-Type': 'application/json' },
+               });
 
-          if (response.ok) {
-               document.location.replace('/');
-          } else {
-               alert('Failed to log in');
+               if (response.ok) {
+                    document.location.reload(true);
+                    document.location.replace('/');
+                    document.location.reload(true);
+               } else {
+                    alert('Failed to log in');
+               }
           }
-     }
-};
+     };
 
-document
-     .querySelector('.login-form')
-     .addEventListener('submit', loginFormHandler);
+     const initApplication = () => {
+          document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+     }
+
+     initApplication();
+
+});
